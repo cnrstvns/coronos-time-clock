e7786d56f0d7f7925f81c6307224c69e2d5b0179
dkruppenbacher <62975981+dkruppenbacher@users.noreply.github.com>
2020-04-01 20:21:01 -0400

    Version 0.2; see revision notes at top of Employee.java

--- Employee.java
+++ Employee.java
@@ -3,7 +3,11 @@ import java.util.Date;
 import org.joda.time.*;
 /**
  * @author Dalton Kruppenbacher
- * @version 0.1
+ * @version 0.2
+ * Revision Notes: Added support for an Employee's position
+ *                 Added YTD totals for Employee wages earned
+ *                 Added username and password fields
+ *                 Added accessors for all employee data
  *
  * ISTE 121.01 CPS:ID2
  * Final Project
@@ -33,6 +37,11 @@ public class Employee {
     private String state;
     private String zipCode;
     private transient String socialSecurityNumber;
+    private String position;
+
+    //global employee account data
+    private transient String userName;
+    private transient String password;
 
     //global payroll data
     private double vacationDaysBalance;
@@ -40,7 +49,7 @@ public class Employee {
     private double hourlyRate;
     private double overtimeRate = hourlyRate * OVERTIME_MULTIPLIER;
     private double totalWagesEarned;
-    private double ytdTotal = 0.0;
+    private double ytdTotal;
 
 
     //global lists for storing items
@@ -58,13 +67,13 @@ public class Employee {
      * @param state An Employee's state, non-abbreviated (ex. New York)
      * @param zipCode An Employee's Zipcode (ex. 12345)
      * @param socialSecurityNumber An Employee's Social Security Number (ex. XXX-XX-XXXX)
+     * @param position An Employee's company position
      * @param hourlyRate An Employee's hourly wage
      *
      * @since 0.1
      */
     public Employee (String firstName, String lastName, String streetAddress, String city, String state, String zipCode,
-                     String socialSecurityNumber, double hourlyRate) {
-        //TODO Data validation for input
+                     String socialSecurityNumber, String position, double hourlyRate) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.streetAddress = streetAddress;
@@ -72,9 +81,13 @@ public class Employee {
         this.state = state;
         this.zipCode = zipCode;
         this.socialSecurityNumber = socialSecurityNumber;
+        this.position = position;
         this.hourlyRate = hourlyRate;
+        userName = firstName + "_" + lastName;
+        password = "password";
         vacationDaysBalance = VACATION_DEFAULT_BALANCE;
         sickDaysBalance = SICK_DEFAULT_BALANCE;
+        ytdTotal = 0.0;
     }//end constructor
 
     /**
@@ -103,8 +116,8 @@ public class Employee {
      * @since 0.1
      */
     public String generateCSV() {
-        return String.format("%s,%s,%s,%s,%s,%s,%s,%2.2f,%2.2f,%2.2f", firstName, lastName, streetAddress, city,
-                state, zipCode, socialSecurityNumber, hourlyRate, vacationDaysBalance, sickDaysBalance);
+        return String.format("%s,%s,%s,%s,%s,%s,%s,%s,%2.2f,%2.2f,%2.2f", firstName, lastName, streetAddress, city,
+                state, zipCode, socialSecurityNumber, position, hourlyRate, vacationDaysBalance, sickDaysBalance);
     }//end generateCSV()
 
     /**
@@ -130,25 +143,160 @@ public class Employee {
     /**
      * Method to calculate wages
      *      Method logic takes into account checking for overtime payment
-     * @return totalWagesEarned The total wages earned for a particular pay period, pre tax and deduction
+     * @return totalWagesEarned The total wages earned for a particular pay period, pre tax and pre deduction
      *
      * @since 0.1
      */
     public double calculateWages() {
-        //TODO Add YTD totals
-        //TODO Add FTD (Fiscal Year to Date) totals
         for(Double hours : shiftTimes) {
             double normalWagesEarned;
             if(hours > STANDARD_HOURS) {
                 double overtimeWagesEarned = (hours - STANDARD_HOURS) * overtimeRate;
                 normalWagesEarned = STANDARD_HOURS * hourlyRate;
                 totalWagesEarned = normalWagesEarned + overtimeWagesEarned;
+                ytdTotal += totalWagesEarned;
             } else {
                 normalWagesEarned = STANDARD_HOURS * hourlyRate;
                 totalWagesEarned = normalWagesEarned;
+                ytdTotal += totalWagesEarned;
             }
         }
         return totalWagesEarned;
     }//end calculateWages()
 
+    /**
+     * Accessor to get an Employee's First Name
+     * @return firstName
+     *
+     * @since 0.2
+     */
+    public String getFirstName() {
+        return firstName;
+    }//end getFirstName()
+
+    /**
+     * Accessor to get an Employee's Last Name
+     * @return lastName
+     *
+     * @since 0.2
+     */
+    public String getLastName() {
+        return lastName;
+    }//end getLastName()
+
+    /**
+     * Accessor to get an Employee's Address
+     * @return streetAddress
+     *
+     * @since 0.2
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }//end getStreetAddress()
+
+    /**
+     * Accessor to get an Employee's City
+     * @return city
+     *
+     * @since 0.2
+     */
+    public String getCity() {
+        return city;
+    }//end getCity()
+
+    /**
+     * Accessor to get an Employee's State
+     * @return state
+     *
+     * @since 0.2
+     */
+    public String getState() {
+        return state;
+    }//end getState()
+
+    /**
+     * Accessor to get an Employee's ZIP Code
+     * @return zipCode
+     *
+     * @since 0.2
+     */
+    public String getZipCode() {
+        return zipCode;
+    }//end getZipCode()
+
+    /**
+     * Accessor to get an Employee's Social Security Number
+     * @return socialSecurityNumber
+     *
+     * @since 0.2
+     */
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }//end getSocialSecurityNumber()
+
+    /**
+     * Accessor to get an Employee's Position
+     * @return position
+     *
+     * @since 0.2
+     */
+    public String getPosition() {
+        return position;
+    }//end getPosition()
+
+    /**
+     * Accessor to get an Employee's Vacation Day Balance
+     * @return vacationDaysBalance
+     *
+     * @since 0.2
+     */
+    public double getVacationDaysBalance() {
+        return vacationDaysBalance;
+    }//end getVacationDaysBalance()
+
+    /**
+     * Accessor to get an Employee's Sick Day Balance
+     * @return sickDaysBalance
+     *
+     * @since 0.2
+     */
+    public double getSickDaysBalance() {
+        return sickDaysBalance;
+    }//end getSickDaysBalance()
+
+    /**
+     * Accessor to get an Employee's Hourly Rate
+     * @return hourlyRate
+     *
+     * @since 0.2
+     */
+    public double getHourlyRate() {
+        return hourlyRate;
+    }//end getHourlyRate()
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    /**
+     * Accessor to get an Employee's Overtime Rate
+     * @return overtimeRate
+     *
+     * @since 0.2
+     */
+    public double getOvertimeRate() {
+        return overtimeRate;
+    }//end getOvertimeRate()
+
+    /**
+     * Accessor to get an Employee's Year-to-Date total
+     * @return ytdTotal
+     *
+     * @since 0.2
+     */
+    public double getYtdTotal() {
+        return ytdTotal;
+    }//end getYtdTotal()
+
 }
+
